c90d306808f8d246fbb8920f9235be8f5ed57998
Dmitry Chigrin <chigrin.dmitry@gmail.com>
2022-12-11 22:45:49 +0500

    feat: add and configure eslint

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 
-console.log('Welcome to the Brain games!');
+import askName from '../src/cli.js';
 
-import { askName } from "../src/cli.js";
+/* eslint-disable no-alert, no-console */
+console.log('Welcome to the Brain games!');
+/* eslint-enable no-alert, no-console */
 
 askName();
