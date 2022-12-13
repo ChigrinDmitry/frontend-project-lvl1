8a7207b0a24b581ff19271f76a49c9be666a7566
Dmitry Chigrin <chigrin.dmitry@gmail.com>
2022-12-13 22:34:25 +0500

    refactor: refactoringn of Check Even game

--- src/checkEven.js
+++ src/checkEven.js
@@ -2,37 +2,43 @@ import readlineSync from 'readline-sync';
 
 /* eslint-disable no-alert, no-console */
 
-const askName = () => {
-  return readlineSync.question('May I have your name? ');
-};
+const askQuestion = (text) => {
+	return readlineSync.question(text);
+}
 
 const isEven = (num) => {
   if (num % 2 === 0) {
     return 'yes';
-  } return 'no';
+  } 
+  return 'no';
 };
 
 const checkEven = () => {
   console.log('Welcome to the Brain games!');
-  const userName = askName();
+  const userName = askQuestion('May I have your name? ');
   console.log(`Hi, ${userName}!`);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-  for (let i = 0; i < 3; i += 1) {
-    const randomNumber = Math.round(Math.random() * 100);
+	let currentAnswersCount = 0;
+	const allAnswersCount = 3;
+
+	while(currentAnswersCount < allAnswersCount ) {
+		const randomNumber = Math.round(Math.random() * 100);
     const correctAnswer = isEven(randomNumber);
 
     console.log(`Question: ${randomNumber}`);
 
-    const answerOfUser = readlineSync.question('Your answer: ');
+    const answerOfUser = askQuestion('Your answer: ');
     if (answerOfUser === correctAnswer) {
       console.log('Correct!');
     } else {
       return console.log(
-        `'yes' is wrong answer ;(. Correct answer was 'no'. Let's try again,  ${userName}!`
+        `'yes' is wrong answer ;(. Correct answer was 'no'. Let's try again, ${userName}!`
       );
     }
-  }
+		currentAnswersCount += 1;
+	}
+
   return console.log(`Congratulations, ${userName}!`);
 };
 
