1ade48cebeec226b62cd5bfcd1cf90737265c7f4
Dmitry Chigrin <chigrin.dmitry@gmail.com>
2022-12-13 22:08:00 +0500

    fix: fixed mistakes in Check Even game

--- src/checkEven.js
+++ src/checkEven.js
@@ -1,52 +1,40 @@
 import readlineSync from 'readline-sync';
 
-/* 
-1. вызов функции в брэйн-ивен
-2. в чек ивен -
-    цикл:
-    1. создаю и потом 
-    показываю случайное число 
-    (рандом)
-    2. если четность числа
-     четное/нечет и он
-    ответил, что четное/нечет - 
-    следующий шаг, 
-    иначе -  
-    - завершаю игру
-    тут, думаю, нужен 
-    ретерн 
-    3.  если все ок - ретерн
-    после цикла
+/* eslint-disable no-alert, no-console */
 
-    // Wait for user's response.
-var userName = readlineSync.question('May I have your name? ');
-console.log('Hi ' + userName + '!');
+const askName = () => {
+  return readlineSync.question('May I have your name? ');
+};
 
-
-*/
+const isEven = (num) => {
+  if (num % 2 === 0) {
+    return 'yes';
+  } return 'no';
+};
 
 const checkEven = () => {
-    console.log('Answer "yes" if the number is even, otherwise answer "no".');
-    for (let i = 0; i < 3; i += 1) {
-        let numberForCheck = Math.random();
-        let correctAnswer;
+  console.log('Welcome to the Brain games!');
+  const userName = askName();
+  console.log(`Hi, ${userName}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-        if (numberForCheck % 2 === 0) {
-            correctAnswer = 'yes'
-        } else {
-            correctAnswer = 'no'
-        }
-        
-        console.log(`Question: ${numberForCheck}`);
+  for (let i = 0; i < 3; i += 1) {
+    const randomNumber = Math.round(Math.random() * 100);
+    const correctAnswer = isEven(randomNumber);
 
-        let answerOfUser = readlineSync.question('Your answer: ')
-        if (answerOfUser === correctAnswer) {
-            console.log ('Congratulations, Bill!') 
-        } else {
-            console.log(`'yes' is wrong answer ;(. Correct answer was 'no'. Let's try again, Bill!`);
-            i = 3;
-        }
+    console.log(`Question: ${randomNumber}`);
+
+    const answerOfUser = readlineSync.question('Your answer: ');
+    if (answerOfUser === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      return console.log(
+        `'yes' is wrong answer ;(. Correct answer was 'no'. Let's try again,  ${userName}!`
+      );
     }
-}
+  }
+  return console.log(`Congratulations, ${userName}!`);
+};
 
 export default checkEven;
+/* eslint-enable no-alert, no-console */
