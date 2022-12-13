20e7e27aae8ab361528ba6fea1122b60cf3e55ad
Dmitry Chigrin <chigrin.dmitry@gmail.com>
2022-12-13 22:37:41 +0500

    refactor: delete duplication of cli.js

--- src/cli.js
+++
@@ -1,8 +0,0 @@
-import readlineSync from 'readline-sync';
-
-const askName = () => {
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hi, ${userName}!`);
-};
-
-export default askName;
